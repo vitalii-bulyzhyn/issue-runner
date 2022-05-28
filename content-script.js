a589441d14fa7e8382d5ebc165b0c83e272fe042
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-05-28 11:47:12 -0500

    Add functionality to move between issues

--- content-script.js
+++ content-script.js
@@ -1,3 +1,6 @@
+let nextUrl;
+let prevUrl;
+
 const leftElement = document.createElement("div")
 const leftTextNode = document.createTextNode("<");
 leftElement.classList.add("next-button")
@@ -5,7 +8,7 @@ leftElement.classList.add("left-position")
 leftElement.appendChild(leftTextNode);
 
 leftElement.onclick = () => {
-    console.log("left click")
+    window.location = prevUrl.html_url
 }
 
 const rightElement = document.createElement("div")
@@ -15,8 +18,21 @@ rightElement.classList.add("right-position")
 rightElement.appendChild(rightTextNode);
 
 rightElement.onclick = () => {
-    console.log("right click")
+    window.location = nextUrl.html_url
 }
 
 document.body.append(leftElement)
 document.body.append(rightElement)
+
+fetch("https://api.github.com/repos/diffgram/diffgram/issues?per_page=100")
+    .then((response) => response.json())
+    .then((data) => {
+        const withPRsRemoved = data.filter(item => !item.html_url.includes('/pull/'))
+        const currentIssueNumber = window.location.pathname.split('/issues/')[1]
+        const currentItem = withPRsRemoved.find(item => item.number === parseInt(currentIssueNumber))
+
+        const indexOfCurrentIssue = withPRsRemoved.indexOf(currentItem)
+
+        nextUrl = withPRsRemoved[indexOfCurrentIssue + 1]
+        prevUrl = withPRsRemoved[indexOfCurrentIssue - 1]
+    });
